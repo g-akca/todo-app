814780f471f3ce6b7bccc4c801a8b22e2de7d91e
g-akca <gok.akca66@gmail.com>
2026-07-12 19:14:32 +0300

    Add text color to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="text-base leading-base bg-navy-950 min-h-screen bg-[url('/images/bg-mobile-dark.jpg')] bg-no-repeat bg-top bg-contain px-6 py-12 flex flex-col gap-10">
+    <div className="text-base leading-base text-purple-600 bg-navy-950 min-h-screen bg-[url('/images/bg-mobile-dark.jpg')] bg-no-repeat bg-top bg-contain px-6 py-12 flex flex-col gap-10">
       <Header />
 
       <MainSection />
