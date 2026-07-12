4c11ae1b149b0d6efe7bf85847e8378abf8ae067
g-akca <gok.akca66@gmail.com>
2026-07-12 19:20:55 +0300

    Implement MainSection layout and create TodoSection inside it

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,12 @@
+import TodoSection from "./TodoSection";
+
 function MainSection() {
   return (
-    <>
-    </>
+    <main className="flex flex-col gap-10">
+      <TodoSection />
+      
+      <p className="text-center text-[14px] leading-base">Drag and drop to reorder list</p>
+    </main>
   )
 }
 
