f013ce0666aa70aebb1397348578241e22316944
g-akca <gok.akca66@gmail.com>
2026-07-12 20:44:55 +0300

    Add base letter spacing to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import MainSection from "./components/MainSection";
 
 function App() {
   return (
-    <div className="text-base leading-base text-purple-600 bg-navy-950 min-h-screen bg-[url('/images/bg-mobile-dark.jpg')] bg-no-repeat bg-top bg-contain px-6 py-12 flex flex-col gap-10">
+    <div 
+      className="
+        text-base leading-base tracking-base text-purple-600 bg-navy-950 min-h-screen bg-[url('/images/bg-mobile-dark.jpg')] 
+        bg-no-repeat bg-top bg-contain px-6 py-12 flex flex-col gap-10
+      "
+    >
       <Header />
 
       <MainSection />
