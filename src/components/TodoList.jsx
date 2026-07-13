7930d23c78e79debba3a393a0735e0396a2f6125
g-akca <gok.akca66@gmail.com>
2026-07-13 23:20:55 +0300

    Add content and styles to TodoList, create TodoItem component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,17 @@
+import TodoItem from "./TodoItem";
+
 function TodoList() {
   return (
-    <>
-    </>
+    <div className="bg-navy-900 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex flex-col">
+      <TodoItem />
+      <TodoItem />
+
+      <div className="py-4 px-5 flex justify-between items-center">
+        <p>5 items left</p>
+
+        <p>Clear Completed</p>
+      </div>
+    </div>
   )
 }
 
