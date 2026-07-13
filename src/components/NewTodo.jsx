e00112dee704ee1880f4cca3506aeb46da27d076
g-akca <gok.akca66@gmail.com>
2026-07-13 08:57:06 +0300

    Give NewTodo content and styles

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,7 +1,10 @@
 function NewTodo() {
   return (
-    <>
-    </>
+    <div className="bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex items-center gap-4">
+      <div className="w-5 aspect-square rounded-full border border-purple-800" />
+
+      <p className="text-gray-600">Create a new todo…</p>
+    </div>
   )
 }
 
--- src/components/TodoSection.jsx
+++ src/components/TodoSection.jsx
@@ -9,7 +9,7 @@ function TodoSection() {
       <div className="flex flex-col gap-4">
         <TodoList />
 
-        <div className="bg-navy-900 h-12 px-4 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex items-center justify-center gap-4 font-bold text-[14px] leading-base">
+        <div className="bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex items-center justify-center gap-4 font-bold text-[14px] leading-base">
           <p className="text-blue-500">All</p>
           <p>Active</p>
           <p>Completed</p>
