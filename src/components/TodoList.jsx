c2cd94b8c628907022f5c253e09cf92648efa178
g-akca <gok.akca66@gmail.com>
2026-07-14 22:34:57 +0300

    Refactor todoItems to use isCompleted instead of completed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,38 +3,38 @@ import TodoItem from "./TodoItem";
 const todoItems = [
   {
     id: 0,
-    completed: true,
+    isCompleted: true,
     task: "Complete online JavaScript course"
   },
   {
     id: 1,
-    completed: false,
+    isCompleted: false,
     task: "Jog around the park 3x"
   },
   {
     id: 2,
-    completed: false,
+    isCompleted: false,
     task: "10 minutes meditation"
   },
   {
     id: 3,
-    completed: false,
+    isCompleted: false,
     task: "Read for 1 hour"
   },
   {
     id: 4,
-    completed: false,
+    isCompleted: false,
     task: "Pick up groceries"
   },
   {
     id: 5,
-    completed: false,
+    isCompleted: false,
     task: "Complete Todo App on Frontend Mentor"
   },
 ];
 
 function TodoList() {
-  const itemsLeft = todoItems.filter(item => !item.completed).length;
+  const itemsLeft = todoItems.filter(item => !item.isCompleted).length;
 
   return (
     <div className="bg-navy-900 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex flex-col">
