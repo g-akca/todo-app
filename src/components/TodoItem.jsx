dbced20318a557dedd2128ba1e2b00227f3af0ae
g-akca <gok.akca66@gmail.com>
2026-07-14 22:52:05 +0300

    Add TodoItem content and styles without completed state

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,18 @@
-function TodoItem() {
+import crossIcon from "/images/icon-cross.svg";
+
+function TodoItem({ id, isCompleted, task }) {
   return (
-    <>
-    </>
+    <div className="py-4 px-5 border-b border-purple-800 flex justify-between items-center gap-4">
+      <div className="flex items-center gap-4">
+        <div className="w-5 aspect-square rounded-full border border-purple-800" />
+
+        <p className="text-purple-100">{task}</p>
+      </div>
+
+      <button type="button">
+        <img src={crossIcon} alt="" className="w-3 aspect-square" />
+      </button>
+    </div>
   )
 }
 
