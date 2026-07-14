a738814be1dd6c57e519fc16c2ae59f8a6416933
g-akca <gok.akca66@gmail.com>
2026-07-14 22:22:29 +0300

    Create todoItems array in TodoList, map it into TodoItem components

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,47 @@
 import TodoItem from "./TodoItem";
 
+const todoItems = [
+  {
+    id: 0,
+    completed: true,
+    task: "Complete online JavaScript course"
+  },
+  {
+    id: 1,
+    completed: false,
+    task: "Jog around the park 3x"
+  },
+  {
+    id: 2,
+    completed: false,
+    task: "10 minutes meditation"
+  },
+  {
+    id: 3,
+    completed: false,
+    task: "Read for 1 hour"
+  },
+  {
+    id: 4,
+    completed: false,
+    task: "Pick up groceries"
+  },
+  {
+    id: 5,
+    completed: false,
+    task: "Complete Todo App on Frontend Mentor"
+  },
+];
+
 function TodoList() {
   return (
     <div className="bg-navy-900 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex flex-col">
-      <TodoItem />
-      <TodoItem />
+      {todoItems.map(item => (
+        <TodoItem
+          key={item.id}
+          {...item}
+        />
+      ))}
 
       <div className="py-4 px-5 flex justify-between items-center">
         <p>5 items left</p>
