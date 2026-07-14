4d775519ac988620d77c35f275f266ca49daf7e6
g-akca <gok.akca66@gmail.com>
2026-07-14 23:03:55 +0300

    Enhance TodoItem component to visually indicate completion status

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,25 @@
 import crossIcon from "/images/icon-cross.svg";
+import checkIcon from "/images/icon-check.svg";
 
 function TodoItem({ id, isCompleted, task }) {
   return (
     <div className="py-4 px-5 border-b border-purple-800 flex justify-between items-center gap-4">
       <div className="flex items-center gap-4">
-        <div className="w-5 aspect-square rounded-full border border-purple-800" />
+        <div 
+          className={`
+            w-5 aspect-square rounded-full
+            ${isCompleted ? 
+              "flex justify-center items-center bg-linear-135 from-[#55DDFF] to-[#C058F3] to-90%" 
+              : "border border-purple-800"
+            }`
+          }
+        >
+          {isCompleted && (
+            <img src={checkIcon} alt="" />
+          )}
+        </div>
 
-        <p className="text-purple-100">{task}</p>
+        <p className={`${isCompleted ? "text-purple-700 line-through" : "text-purple-100"}`}>{task}</p>
       </div>
 
       <button type="button">
