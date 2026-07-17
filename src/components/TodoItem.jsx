91ab14837f29e9af704aeca1b941cf93477b17ba
g-akca <gok.akca66@gmail.com>
2026-07-17 14:13:06 +0300

    Add shrink-0 class to icon and button in TodoItem for visual consistency

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -7,7 +7,7 @@ function TodoItem({ id, isCompleted, task }) {
       <div className="flex items-center gap-4">
         <div 
           className={`
-            w-5 aspect-square rounded-full
+            shrink-0 w-5 aspect-square rounded-full
             ${isCompleted ? 
               "flex justify-center items-center bg-linear-135 from-[#55DDFF] to-[#C058F3]" 
               : "border border-purple-800"
@@ -22,8 +22,8 @@ function TodoItem({ id, isCompleted, task }) {
         <p className={`${isCompleted ? "text-purple-700 line-through" : "text-purple-100"}`}>{task}</p>
       </div>
 
-      <button type="button">
-        <img src={crossIcon} alt="" className="w-3 aspect-square" />
+      <button type="button" className="shrink-0">
+        <img src={crossIcon} alt="Remove icon" className="w-3 aspect-square" />
       </button>
     </div>
   )
