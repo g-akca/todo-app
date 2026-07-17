74ba52f49d085e13054c154fa8a03cfe8c095d44
g-akca <gok.akca66@gmail.com>
2026-07-17 23:39:16 +0300

    Refactor TabList to use dynamic tabs from an array instead of static paragraphs

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 
+const tabs = [
+  {
+    index: 0,
+    name: "All"
+  },
+  {
+    index: 1,
+    name: "Active"
+  },
+  {
+    index: 2,
+    name: "Completed"
+  },
+];
+
 function TabList() {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <div className="flex items-center gap-4 font-bold text-[14px] leading-base">
-      <p>All</p>
-      <p>Active</p>
-      <p>Completed</p>
+      {tabs.map(tab => (
+        <button
+          key={tab.index}
+          type="button"
+          onClick={() => setSelectedIndex(tab.index)}
+          className={`cursor-pointer transition-all ${selectedIndex === tab.index ? "text-blue-500" : "hover:text-purple-300"}`}
+        >
+          {tab.name}
+        </button>
+      ))}
     </div>
   )
 }
