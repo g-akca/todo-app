704f709f5cfbcc0f0c70dc673cdb6a0f7b58f2d9
g-akca <gok.akca66@gmail.com>
2026-07-17 23:01:08 +0300

    Add tablet responsive styling to LoginWrapper

--- src/components/LoginWrapper.jsx
+++ src/components/LoginWrapper.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router";
 
 function LoginWrapper() {
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-10 tablet:gap-6">
       <LoginSection />
       
-      <p className="text-center text-[14px] leading-base">First time here? <Link to="/signup" className="text-blue-500 font-semibold">Sign Up</Link></p>
+      <p className="text-center text-[14px] leading-base tablet:text-[16px]">
+        First time here? <Link to="/signup" className="text-blue-500 font-semibold">Sign Up</Link>
+      </p>
     </div>
   )
 }
