d9637057feedd2ee73145199ffbe9b1bdfb50bc2
g-akca <gok.akca66@gmail.com>
2026-07-17 21:59:35 +0300

    Refactor App component layout for background styling, add tablet styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   return (
     <div 
       className="
-        text-base leading-base tracking-base text-purple-600 bg-navy-950 min-h-screen 
-        bg-[url('/images/bg-mobile-dark.jpg')] bg-no-repeat bg-top bg-contain px-6 py-12 flex justify-center
+        relative text-base leading-base tracking-base text-purple-600
+        bg-navy-950 min-h-screen px-6 py-12 flex justify-center
       "
     >
-      <div className="flex flex-col gap-10 grow max-w-135.25">
+      <div 
+        className="
+          absolute inset-x-0 top-0 h-50 bg-[url('/images/bg-mobile-dark.jpg')] bg-cover
+          bg-top-left bg-no-repeat tablet:h-75 tablet:bg-[url('/images/bg-desktop-dark.jpg')]
+        "
+      />
+      
+      <div className="relative z-10 flex flex-col gap-10 grow max-w-135.25 tablet:gap-12">
         <Header />
       
         <Routes>
