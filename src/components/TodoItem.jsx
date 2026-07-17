e80e28615cddc0cafc492a5d5862f88fa0ca09b5
g-akca <gok.akca66@gmail.com>
2026-07-17 22:56:29 +0300

    Add tablet responsive styling to TodoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,11 +3,11 @@ import CheckIcon from "./icons/CheckIcon";
 
 function TodoItem({ id, isCompleted, task }) {
   return (
-    <div className="py-4 px-5 border-b border-purple-800 flex justify-between items-center gap-4">
-      <div className="flex items-center gap-4">
+    <div className="py-4 px-5 border-b border-purple-800 flex justify-between items-center gap-4 tablet:p-6">
+      <div className="flex items-center gap-4 tablet:gap-6">
         <div 
           className={`
-            shrink-0 w-5 aspect-square rounded-full
+            shrink-0 w-5 aspect-square rounded-full tablet:w-6
             ${isCompleted ? 
               "flex justify-center items-center bg-linear-135 from-[#55DDFF] to-[#C058F3]" 
               : "border border-purple-800"
@@ -19,11 +19,18 @@ function TodoItem({ id, isCompleted, task }) {
           )}
         </div>
 
-        <p className={`${isCompleted ? "text-purple-700 line-through" : "text-purple-100"}`}>{task}</p>
+        <p 
+          className={`
+            mt-px tablet:text-[18px] tablet:leading-base tablet:mt-1 
+            ${isCompleted ? "text-purple-700 line-through" : "text-purple-100"}
+          `}
+        >
+          {task}
+        </p>
       </div>
 
       <button type="button" className="shrink-0">
-        <img src={crossIcon} alt="Remove icon" className="w-3 aspect-square" />
+        <img src={crossIcon} alt="Remove icon" className="w-3 aspect-square tablet:w-4.25" />
       </button>
     </div>
   )
