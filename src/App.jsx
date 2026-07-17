fb329eb6b045cbf97453621c6ab336edc1d22252
g-akca <gok.akca66@gmail.com>
2026-07-17 14:08:56 +0300

    Refactor App component layout for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ function App() {
   return (
     <div 
       className="
-        text-base leading-base tracking-base text-purple-600 bg-navy-950 min-h-screen bg-[url('/images/bg-mobile-dark.jpg')] 
-        bg-no-repeat bg-top bg-contain px-6 py-12 flex flex-col gap-10
+        text-base leading-base tracking-base text-purple-600 bg-navy-950 min-h-screen 
+        bg-[url('/images/bg-mobile-dark.jpg')] bg-no-repeat bg-top bg-contain px-6 py-12 flex justify-center
       "
     >
-      <Header />
+      <div className="flex flex-col gap-10 grow max-w-135.25">
+        <Header />
       
-      <Routes>
-        <Route path="/" element={<MainSection />} />
+        <Routes>
+          <Route path="/" element={<MainSection />} />
 
-        <Route path="/login" element={<LoginWrapper />} />
+          <Route path="/login" element={<LoginWrapper />} />
 
-        <Route path="/signup" element={<SignupWrapper />} />
-      </Routes>
+          <Route path="/signup" element={<SignupWrapper />} />
+        </Routes>
+      </div>
     </div>
   )
 }
