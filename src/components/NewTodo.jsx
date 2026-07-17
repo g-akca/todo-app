26809890ca6cb84cdb0b459e8f72b2a221b70390
g-akca <gok.akca66@gmail.com>
2026-07-17 22:39:31 +0300

    Add tablet responsive styling to NewTodo component

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,9 +1,14 @@
 function NewTodo() {
   return (
-    <div className="bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex items-center gap-4">
-      <div className="w-5 aspect-square rounded-full border border-purple-800" />
+    <div 
+      className="
+        bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] 
+        flex items-center gap-4 tablet:h-16 tablet:gap-6
+      "
+    >
+      <div className="w-5 aspect-square rounded-full border border-purple-800 tablet:w-6" />
 
-      <p className="text-gray-600">Create a new todo…</p>
+      <p className="text-gray-600 mt-px tablet:text-[18px] tablet:leading-base tablet:mt-1">Create a new todo…</p>
     </div>
   )
 }
