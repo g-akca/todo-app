3ea37f991cdfa23501448f517c195519520e50dc
g-akca <gok.akca66@gmail.com>
2026-07-17 22:25:53 +0300

    Create TabSection component and add tablet responsive styling in TodoSection

---
+++ src/components/TabSection.jsx
@@ -0,0 +1,11 @@
+function TabSection() {
+  return (
+    <div className="flex items-center gap-4 font-bold text-[14px] leading-base">
+      <p className="text-blue-500">All</p>
+      <p>Active</p>
+      <p>Completed</p>
+    </div>
+  )
+}
+
+export default TabSection;
--- src/components/TodoSection.jsx
+++ src/components/TodoSection.jsx
@@ -1,18 +1,22 @@
 import TodoList from "./TodoList";
 import NewTodo from "./NewTodo";
+import TabSection from "./TabSection";
 
 function TodoSection() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 tablet:gap-6">
       <NewTodo />
 
       <div className="flex flex-col gap-4">
         <TodoList />
 
-        <div className="bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] flex items-center justify-center gap-4 font-bold text-[14px] leading-base">
-          <p className="text-blue-500">All</p>
-          <p>Active</p>
-          <p>Completed</p>
+        <div 
+          className="
+            bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] 
+            flex items-center justify-center tablet:hidden
+          "
+        >
+          <TabSection />
         </div>
       </div>
     </div>
