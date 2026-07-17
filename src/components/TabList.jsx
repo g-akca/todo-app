524f8cb67d7f3b13348b131b69866ed3fdd61499
g-akca <gok.akca66@gmail.com>
2026-07-17 23:34:02 +0300

    Create selectedIndex state in TabList

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 function TabList() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <div className="flex items-center gap-4 font-bold text-[14px] leading-base">
-      <p className="text-blue-500">All</p>
+      <p>All</p>
       <p>Active</p>
       <p>Completed</p>
     </div>
