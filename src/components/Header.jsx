b2e21d952982f4f14f50e67deeeef77c808d1278
g-akca <gok.akca66@gmail.com>
2026-07-17 22:18:43 +0300

    Add tablet responsive styling to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,16 @@ import sunIcon from "/images/icon-sun.svg";
 function Header() {
   return (
     <header className="flex justify-between items-center gap-4">
-      <h1 className="uppercase text-white tracking-[10px] text-[26px] leading-base font-bold mt-1.5">Todo</h1>
+      <h1 
+        className="
+          uppercase text-white tracking-[10px] text-[26px] leading-base 
+          font-bold mt-1.5 tablet:text-[40px] tablet:tracking-[14px]
+        "
+      >
+        Todo
+      </h1>
 
-      <img src={sunIcon} alt="Sun icon" className="w-5 aspect-square" />
+      <img src={sunIcon} alt="Sun icon" className="w-5 aspect-square tablet:w-6.5" />
     </header>
   )
 }
