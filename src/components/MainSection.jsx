867d69f28e9b091b8324710b5c0c7be2bdd4eec3
g-akca <gok.akca66@gmail.com>
2026-07-17 22:20:52 +0300

    Add tablet responsive styling to MainSection

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,7 +2,7 @@ import TodoSection from "./TodoSection";
 
 function MainSection() {
   return (
-    <main className="flex flex-col gap-10">
+    <main className="flex flex-col gap-10 tablet:gap-6">
       <TodoSection />
       
       <p className="text-center text-[14px] leading-base">Drag and drop to reorder list</p>
