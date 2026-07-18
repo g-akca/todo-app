122497fe529abbffee9f62ebf23709b11f8f1fcd
g-akca <gok.akca66@gmail.com>
2026-07-18 11:23:25 +0300

    Turn NewTodo into a form

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,6 +1,6 @@
 function NewTodo() {
   return (
-    <div 
+    <form 
       className="
         bg-navy-900 h-12 px-6 rounded-[5px] shadow-[0_35px_50px_rgba(0,0,0,0.5)] 
         flex items-center gap-4 tablet:h-16 tablet:gap-6
@@ -8,8 +8,13 @@ function NewTodo() {
     >
       <div className="w-5 aspect-square rounded-full border border-purple-800 tablet:w-6" />
 
-      <p className="text-gray-600 mt-px tablet:text-[18px] tablet:leading-base tablet:mt-1">Create a new todo…</p>
-    </div>
+      <input 
+        type="text"
+        id="todo"
+        placeholder="Create a new todo…"
+        className="w-full py-1 text-gray-600 mt-px placeholder:text-gray-600 tablet:text-[18px] tablet:leading-base tablet:mt-0.5"
+      />
+    </form>
   )
 }
 
