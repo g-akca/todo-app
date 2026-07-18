be1e4be704a50ec1c71ec81e930b32cc224ad561
g-akca <gok.akca66@gmail.com>
2026-07-18 15:43:48 +0300

    Initialize backend with Express server and centralized error handling

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Centralized error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+});
+
+app.listen(PORT, (e) => {
+  if (e) {
+    throw error;
+  }
+
+  console.log(`Listening on port ${PORT}!`)
+});
