a9d40f90c98cbb1b07b7472a3ac52e9da435499e
g-akca <gok.akca66@gmail.com>
2026-07-19 12:18:36 +0300

    Move AuthProvider to main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,42 +4,39 @@ import LoginWrapper from "./components/LoginWrapper";
 import SignupWrapper from "./components/SignupWrapper";
 import MainSection from "./components/MainSection";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
 import { Routes, Route, Navigate } from "react-router";
 
 function App() {
   return (
-    <AuthProvider>
-      <div 
-        className="
-          relative text-base leading-base tracking-base text-purple-600 bg-navy-950 px-6 py-12
-          min-h-screen flex justify-center tablet:text-[14px] light:text-gray-600 light:bg-gray-50
-        "
-      >
-        <BackgroundImage />
-        
-        <div className="relative z-10 flex flex-col gap-10 grow max-w-135.25 tablet:gap-12">
-          <Header />
-        
-          <Routes>
-            <Route 
-              path="/" 
-              element={
-                <ProtectedRoute>
-                  <MainSection />
-                </ProtectedRoute>
-              } 
-            />
+    <div 
+      className="
+        relative text-base leading-base tracking-base text-purple-600 bg-navy-950 px-6 py-12
+        min-h-screen flex justify-center tablet:text-[14px] light:text-gray-600 light:bg-gray-50
+      "
+    >
+      <BackgroundImage />
+      
+      <div className="relative z-10 flex flex-col gap-10 grow max-w-135.25 tablet:gap-12">
+        <Header />
+      
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <ProtectedRoute>
+                <MainSection />
+              </ProtectedRoute>
+            } 
+          />
 
-            <Route path="/login" element={<LoginWrapper />} />
+          <Route path="/login" element={<LoginWrapper />} />
 
-            <Route path="/signup" element={<SignupWrapper />} />
-            
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
+          <Route path="/signup" element={<SignupWrapper />} />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
-    </AuthProvider>
+    </div>
   )
 }
 
