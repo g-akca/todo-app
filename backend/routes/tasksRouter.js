869385d9e705aaaba12c9bc40196591eecbc3f00
g-akca <gok.akca66@gmail.com>
2026-07-21 22:10:16 +0300

    Implement GET endpoint for retrieving tasks by user

--- backend/routes/tasksRouter.js
+++ backend/routes/tasksRouter.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { getTasksByUserId } from "../db/queries.js";
 
 const tasksRouter = Router();
+
+tasksRouter.get("/", async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const tasks = await getTasksByUserId(req.user.id);
+
+    return res.status(200).json({ tasks });
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default tasksRouter;
